cd50338d832a4937351ce7c2f4efccc56608cd33
yjrudrr <yjrodrr@gmail.com>
2026-06-03 13:05:01 +0200

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,1 +1,7 @@
-
+const W=900,H=600,R=22;
+const MAX_HP={sword:10,bow:8,shield:16};
+const SHIELD_CYCLE=4.0,SHIELD_DURATION=4.0;
+const SWORD_LEN=68,SWORD_W=5;
+const ARROW_INTERVAL=1.8;
+const WALL_BOUNCE=0.95,PUSH_SCALE=280;
+const ALL_WEAPONS=['sword','bow','shield'];
