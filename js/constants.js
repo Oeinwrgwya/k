6f4cde841cb005118aaa5af014c9cd125b7a72ef
yjrudrr <yjrodrr@gmail.com>
2026-07-10 17:44:06 +0200

    Delete js directory

--- js/constants.js
+++
@@ -1,7 +0,0 @@
-const W=900,H=600,R=22;
-const MAX_HP={sword:120,bow:80,shield:160};
-const SHIELD_CYCLE=4.0,SHIELD_DURATION=4.0;
-const SWORD_LEN=68,SWORD_W=5;
-const ARROW_INTERVAL=2.1;
-const WALL_BOUNCE=0.95,PUSH_SCALE=280;
-const ALL_WEAPONS=['sword','bow','shield'];
